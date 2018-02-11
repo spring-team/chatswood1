79e8fd2e4110d220c75736124f8cd479a7bbcd59
Rod Johnson <johnsonroda@gmail.com>
2018-02-11 06:12:18 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/c1/Chatswood1Application.java
@@ -0,0 +1,12 @@
+package com.atomist.c1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatswood1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatswood1Application.class, args);
+	}
+}
---
+++ src/test/java/com/atomist/c1/Chatswood1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atomist.c1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chatswood1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
